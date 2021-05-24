8a5bf02a0350ee879c3622b863b2cead714f7b10
Eds <steadyede@live.co.uk>
2021-05-24 09:35:30 +0100

    graphql package added

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 export default function App() {
   return (
+  <ApolloProvider client={client}>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  </ApolloProvider>
   );
 }
 
