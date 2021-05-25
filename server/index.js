89d92f0e55b850c4f6d069b7a65f0cec846db373
Eds <steadyede@live.co.uk>
2021-05-25 17:22:16 +0100

    dummy data for events

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 2021;
 
-const userData = require("../data/events.json")
+const eventData = require("../data/events.json")
 const graphql = require("graphql")
 const {
   GraphQLObjectType,
@@ -13,9 +13,50 @@ const {
 } = graphql;
 const { graphqlHTTP } = require("express-graphql")
 
-const RootQuery = "query"
-const Mutation = "mutation"
+const EventType = new GraphQLObjectType({
+  name: "EventQueryType",
+  fields: () => ({
+    id: { type: GraphQLInt},
+    name: { type: GraphQLString},
+    date: { type: GraphQLString},
+    location: { type: GraphQLString},
+    venue: { type: GraphQLString},
+    wiki_link: { type: GraphQLString},
+  })
+})
 
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllEvents: {
+      type: new GraphQLList(EventType),
+      args: { id: { type: GraphQLInt }},
+      resolve(parent, args) {
+        return eventData // see all data in project
+      }
+    }
+  }
+})
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createEvent: {
+      type: EventType,
+      args: {
+        name: { type: GraphQLString},
+        date: { type: GraphQLString},
+        location: { type: GraphQLString},
+        venue: { type: GraphQLString},
+        wiki_link: { type: GraphQLString},
+      },
+      resolve(parent, args) {
+        userData.push({id: eventData.length + 1, name: args.name, date: args.date, location: args.location, venue: args.venue, wiki_link: args.wiki_link })
+        return args
+      }
+    }
+  }
+})
 
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation })
 
