1cc30853d5a7e0c5d6db6001e676d3788ed79e6c
Eds <steadyede@live.co.uk>
2021-05-26 08:49:56 +0100

    apollo provider connects to graphql api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 2021;
+const PORT = 5000;
 
 const { graphqlHTTP } = require("express-graphql")
 const schema = require("./Schemas/index")
